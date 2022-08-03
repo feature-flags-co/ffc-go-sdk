c24d4053fc64ec92e7c72e728a6db01897934d2c
zhanghongtao <hadizhang@tencent.com>
2022-08-04 07:32:18 +0800

    streaming builder

--- streaming/streaming_builder.go
+++ streaming/streaming_builder.go
@@ -1,1 +1,30 @@
 package streaming
+
+import "time"
+
+type StreamingBuilder struct {
+	StreamingURI    string
+	FirstRetryDelay time.Duration
+	MaxRetryTimes   int64
+}
+
+func NewStreamingBuilder() *StreamingBuilder {
+	builder := StreamingBuilder{
+	}
+	return &builder
+}
+
+func (s *StreamingBuilder) newStreamingURI(uri string) *StreamingBuilder {
+	s.StreamingURI = uri
+	return s
+}
+
+func (s *StreamingBuilder) firstRetryDelay(duration time.Duration) *StreamingBuilder {
+	s.firstRetryDelay(duration)
+	return s
+}
+
+func (s *StreamingBuilder) maxRetryTimes(maxRetryTimes int64) *StreamingBuilder {
+	s.MaxRetryTimes = maxRetryTimes
+	return s
+}
